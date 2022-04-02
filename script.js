7a87a648fa948016ebd78e5c6c6bc795f6400895
Elisa M <elisa.m.makinen@gmail.com>
2022-04-02 04:39:00 -0400

    seventh version

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const landingImage = document.querySelector('.landing-image');
 const link1 = document.querySelector('.link-image-1');
 const link2 = document.querySelector('.link-image-2');
 const link3 = document.querySelector('.link-image-3');
+const pageLinks = document.querySelector('.page-links-container');
+
+link1.addEventListener('mouseover', showImage1);
+link2.addEventListener('mouseover', showImage2);
+link3.addEventListener('mouseover', showImage3);
+pageLinks.addEventListener('mouseleave', showLandingImage);
 
 // Navbar
 
@@ -28,33 +34,27 @@ function scrollFunction() {
   }
 }
 */
-link1.addEventListener('mouseover', showImage1);
-link1.addEventListener('mouseleave', showLandingImage);
-link2.addEventListener('mouseover', showImage2);
-link2.addEventListener('mouseleave', showLandingImage);
-link3.addEventListener('mouseover', showImage3);
-link3.addEventListener('mouseleave', showLandingImage);
 
 iconchange.addEventListener('click', hei);
 
 function showImage1() {
   landingImage.style.backgroundImage = "url('IMG/perhe-kuvaus.jpg')";
-  landingImage.style.transition = 'all 300ms';
+  landingImage.style.transition = 'all 400ms';
 }
 
 function showImage2() {
   landingImage.style.backgroundImage = "url('IMG/muoto-kuvaus.jpg')";
-  landingImage.style.transition = 'all 300ms';
+  landingImage.style.transition = 'all 400ms';
 }
 
 function showImage3() {
   landingImage.style.backgroundImage = "url('IMG/perhe-kuvaus.jpg')";
-  landingImage.style.transition = 'all 300ms';
+  landingImage.style.transition = 'all 400ms';
 }
 
 function showLandingImage() {
   landingImage.style.backgroundImage = "url('IMG/odotus-kuvaus.jpg')";
-  landingImage.style.transition = 'all 1000ms';
+  landingImage.style.transition = 'all 600ms';
 }
 
 function hei() {
